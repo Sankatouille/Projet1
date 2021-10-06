2502c9113e7f39b8d23de7a2cd6d07f70d9f29ee
d.chantreau <chantreaudamien@gmail.com>
2021-10-06 11:57:28 +0200

    add contactform into the new file contact.html

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-/*const message=
+const message=
 "Thank you for submitting your order.Your favorite Food Truck will prepare that!"
 
 document
 .getElementById("Formulaire")
 addEventListener("submit",function (event){
   event.preventDefault();
-  alert(message);});*/
+  alert(message);});
